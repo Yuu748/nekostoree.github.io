0f9806d14e6885c5c2fe093e35ffc4e12019407c
nekostoree <Yuuneko76@gmail.com>
2026-07-16 05:32:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ import {
     getDocs
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBwmY0eghiVz1njptpM_4ylk6DTO15EpCI",
+  authDomain: "neko-store-9dbd1.firebaseapp.com",
+  projectId: "neko-store-9dbd1",
+  storageBucket: "neko-store-9dbd1.firebasestorage.app",
+  messagingSenderId: "373922683235",
+  appId: "1:373922683235:web:5790e3f35f5970a94d5b56"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
 const groupBtn = document.querySelector(".group-btn");
 const dropdown = document.querySelector(".dropdown");
 
