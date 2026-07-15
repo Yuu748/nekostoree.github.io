29c3c057aa4242b84dc1a42bb9e4f0b822f5cced
nekostoree <Yuuneko76@gmail.com>
2026-07-16 00:56:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ if (groupBtn && dropdown) {
         dropdown.classList.toggle("active");
     });
 }
+
+const cat = document.getElementById("catMascot");
+const sound = document.getElementById("catSound");
+
+cat.addEventListener("click", () => {
+
+    sound.pause();
+    sound.currentTime = 0;
+    sound.play();
+
+    cat.src = "cat_meow.png";
+
+    cat.classList.add("active");
+
+    setTimeout(() => {
+
+        cat.src = "cat_idle.png";
+
+        cat.classList.remove("active");
+
+    },700);
+
+});
