e8f05430547ebbd02d697e971dff8030c395a994
nekostoree <Yuuneko76@gmail.com>
2026-09-13 21:54:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -81,20 +81,6 @@ if (instagramBtn && videoPopup && promoVideo && closeVideo) {
 
 }
 
-videoPopup.addEventListener("click",(e)=>{
-
-    if(e.target===videoPopup){
-
-        promoVideo.pause();
-
-        promoVideo.currentTime=0;
-
-        videoPopup.classList.remove("show");
-
-    }
-
-});
-
 async function loadStock(){
 
     const stockContainer = document.getElementById("stockContainer");
